a48ae8746897134a60e3fe3b681ee15d83d3fb04
cecnull1 <null114514cec@163.com>
2025-03-22 23:36:47 +0800

    清空导入

--- src/main/kotlin/a/b/noride2_changed_compatible/Noride2Changed.kt
+++ src/main/kotlin/a/b/noride2_changed_compatible/Noride2Changed.kt
@@ -3,7 +3,6 @@ package a.b.noride2_changed_compatible
 import a.b.noride2_changed_compatible.enchantment.QiangZhiTransfur
 import a.b.noride2_changed_compatible.enchantment.YongJiuShouHua
 import constant.Constant
-import net.minecraftforge.fml.ModList
 import net.minecraftforge.fml.common.Mod
 
 @Mod(Constant.MODDatas.Noride2Changed.MOD_ID)
